c4c490a55498749d29918aa60c2e710a1c82cfa2
prafullakinkar2025 <prafullakinkar@shreeinfotechsoft.com>
2025-10-30 13:11:49 +0530

    s6

--- submit.php
+++ submit.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 // Database connection details
 $servername = "localhost";
 $username = "shreeinfotechsof";       // or your cPanel DB username
